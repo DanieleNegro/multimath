e7a686aa82bd48a99a8207adfd9d237a0e8fd849
DanieleNegro <danieleweb1987@gmail.com>
2024-10-03 12:09:34 +0200

    fix index html reference

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -46,6 +46,7 @@ export default (env) => {
         template: "./public/index.html",
         filename: "./index.html",
         favicon: "./public/favicon.ico",
+        manifest: "./public/manifest.json",
       }),
     ],
   };
