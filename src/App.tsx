99dedddd050fd281ca78be62d9094c7554d61f03
DanieleNegro <danieleweb1987@gmail.com>
2024-10-07 14:30:33 +0200

    improve by lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import MultiMath from "./components/MultiMath";
 import { Route, Routes } from "react-router-dom";
 import Navabar from "./components/navbar/Navbar";
+import WelcomeHome from "./components/home/WelcomeHome";
+import Spinner from "./shared/Spinner";
+
+const MultiMath = lazy(() => import("./components/MultiMath"));
 
 const App: React.FC = () => {
   return (
     <div className="App container-fluid mx-0 px-0">
       <Navabar />
       <Routes>
-        <Route path="/multimath" element={<MultiMath />} />
+        <Route path="/" element={<WelcomeHome />} />
+        <Route
+          path="/multimath"
+          element={
+            <Suspense fallback={<Spinner />}>
+              <MultiMath />
+            </Suspense>
+          }
+        />
       </Routes>
     </div>
   );
---
+++ src/components/home/WelcomeHome.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const WelcomeHome: React.FC = () => {
+  return (
+    <div className="container-fluid h-100 mt-5">
+      <div className="d-flex flex-column text-center">
+        <h1>Welcome home in this React Typescript App</h1>
+        <h2>This app has two pages:</h2>
+      </div>
+      <div className="d-flex justify-content-center">
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <div className="d-flex align-items-center">
+              <h4>MultiMath: enjoy with multiplication table</h4>
+              <Link className="nav-link active m-0 ps-3" to="/multimath">
+                Try me!
+              </Link>
+            </div>
+          </li>
+          <li className="list-group-item">
+            <h4>Library manager</h4>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomeHome;
