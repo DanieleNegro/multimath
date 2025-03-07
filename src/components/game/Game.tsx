3b2cb5fc59bb0840d1f7d7c5af4be133e62561ab
DanieleNegro <danieleweb1987@gmail.com>
2025-03-07 15:27:57 +0100

    handle message

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -3,9 +3,11 @@ import "../../App.css";
 import "./Game.css";
 import { usePlayerContext } from "../../context/PlayerContext";
 import { useTranslation } from "react-i18next";
+import { useMessageContext } from "../../context/MessageContext";
 
 const Game: React.FC = () => {
   const { t } = useTranslation(["multimathPage"]);
+  const { setText } = useMessageContext();
   const { result, results, setResults } = usePlayerContext();
   const inputsRef = useRef<HTMLInputElement[]>([]);
   const [listFault, setListFault] = useState<number[]>([]);
@@ -34,35 +36,35 @@ const Game: React.FC = () => {
     return list;
   };
 
+  const onSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const listFault: number[] = [];
+    let score = 0;
+    for (let i = 0; i < result.problemCount; i++) {
+      const answer: number = inputsRef.current[i].valueAsNumber;
+      if (i * result.factor === answer) {
+        score++;
+      } else {
+        listFault.push(i);
+      }
+    }
+    setListFault(listFault);
+    setResults([
+      ...results,
+      {
+        playerName: result.playerName,
+        score: score,
+        problemCount: result.problemCount,
+        factor: result.factor,
+      },
+    ]);
+    setText(listFault.length > 0 ? "Looser!" : "Winner!");
+  };
+
   return (
     result.problemCount > 0 && (
       <div className="container-fluid d-flex mt-5">
-        <form
-          className="form-horizontal w-100"
-          onSubmit={(event: React.SyntheticEvent<HTMLFormElement>) => {
-            event.preventDefault();
-            const listFault: number[] = [];
-            let score = 0;
-            for (let i = 0; i < result.problemCount; i++) {
-              const answer: number = inputsRef.current[i].valueAsNumber;
-              if (i * result.factor === answer) {
-                score++;
-              } else {
-                listFault.push(i);
-              }
-            }
-            setListFault(listFault);
-            setResults([
-              ...results,
-              {
-                playerName: result.playerName,
-                score: score,
-                problemCount: result.problemCount,
-                factor: result.factor,
-              },
-            ]);
-          }}
-        >
+        <form className="form-horizontal w-100" onSubmit={onSubmit}>
           {buildForm()}
           <div className="form-group d-flex">
             <div className="col-sm-2" />
